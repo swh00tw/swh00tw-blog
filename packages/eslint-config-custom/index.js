bcda8ccc796d1dd7410346234ed7f6cb4b3c250c
swh00tw <a6140000@gmail.com>
2023-07-22 23:25:14 +0800

    add unused import rule

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -7,9 +7,21 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
   ],
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "unused-imports"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
   },
   parserOptions: {
     babelOptions: {
