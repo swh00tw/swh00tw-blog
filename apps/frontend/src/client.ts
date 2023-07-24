21b28af026480e3b237afd29accdbec1fc73bd27
swh00tw <a6140000@gmail.com>
2023-07-25 00:59:22 +0800

    fix(frontend): houdini client: use env

--- apps/frontend/src/client.ts
+++ apps/frontend/src/client.ts
@@ -1,7 +1,8 @@
 import { HoudiniClient } from "$houdini";
+import { PUBLIC_FRONTEND_GQL_API_ENDPOINT } from "$env/static/public";
 
 export default new HoudiniClient({
-	url: "http://localhost:3000/api/graphql"
+	url: PUBLIC_FRONTEND_GQL_API_ENDPOINT
 	// uncomment this to configure the network call (for things like authentication)
 	// for more information, please visit here: https://www.houdinigraphql.com/guides/authentication
 	// fetchParams({ session }) {
