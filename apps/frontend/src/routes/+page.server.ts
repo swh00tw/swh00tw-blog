862f0132b0c2e78b667fb4fcc9575ffc2af605fe
swh00tw <a6140000@gmail.com>
2023-07-24 23:27:15 +0800

    remove urql svelte

--- apps/frontend/src/routes/+page.server.ts
+++ apps/frontend/src/routes/+page.server.ts
@@ -1,29 +1,5 @@
 import type { PageServerLoad } from "./$types";
-import { getGraphQLClient } from "$lib/graphql";
-import { gql } from "@urql/svelte";
-
-const getAllPosts = gql`
-	query Test {
-		Posts {
-			docs {
-				title
-				id
-			}
-		}
-	}
-`;
 
 export const load: PageServerLoad = async ({ params, fetch }) => {
-	const client = getGraphQLClient({ fetch });
-	const queryResult = await client
-		.query(getAllPosts, {})
-		.toPromise()
-		.then((res) => res.data)
-		.catch((err) => {
-			console.error(err);
-		});
-
-	return {
-		queryResult
-	};
+	return {};
 };
