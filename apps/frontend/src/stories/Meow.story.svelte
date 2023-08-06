e02708ac8f4d97465fde62c486511e2a27991339
swh00tw <a6140000@gmail.com>
2023-08-06 15:12:38 +0800

    feat(histoire): add and init histoire

---
+++ apps/frontend/src/stories/Meow.story.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	// for testing
+	import type { Hst } from "@histoire/plugin-svelte";
+	export let Hst: Hst;
+
+	let disabled = false;
+	let content = "Meow";
+</script>
+
+<Hst.Story title="🐱 Meow">
+	<Hst.Variant title="test"
+		>🐱
+		<svelte:fragment slot="controls">
+			<Hst.Text bind:value={content} title="Content" />
+			<Hst.Checkbox bind:value={disabled} title="Disabled" />
+		</svelte:fragment></Hst.Variant
+	>
+</Hst.Story>
