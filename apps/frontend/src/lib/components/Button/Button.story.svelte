02e49acb8a2ddfd878fc9fe378720d9302dbd4fe
swh00tw <a6140000@gmail.com>
2023-08-12 22:11:47 -0400

    feat(button): add button

---
+++ apps/frontend/src/lib/components/Button/Button.story.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import type { Hst } from "@histoire/plugin-svelte";
+	import Button from "$lib/components/Button/index.svelte";
+	import { cn } from "@/lib/cn";
+	export let Hst: Hst;
+
+	let content = "Share";
+	let copied = false;
+</script>
+
+<Hst.Story title="Button">
+	<svelte:fragment slot="controls">
+		<Hst.Text bind:value={content} title="Content" />
+	</svelte:fragment>
+	<Hst.Variant title="primary">
+		<Button className={cn("m-4", "text-[16px]")}>{content}</Button>
+	</Hst.Variant>
+	<Hst.Variant title="accent">
+		<Button className={cn("m-4", "text-[16px]")} variant="accent">{content}</Button>
+	</Hst.Variant>
+	<Hst.Variant title="copy/paste">
+		<Button
+			className={cn("m-4", "text-[16px]")}
+			variant={copied ? "accent" : "primary"}
+			on:click={() => {
+				copied = !copied;
+				content = copied ? "Copied!" : "Share";
+			}}>{content}</Button
+		>
+	</Hst.Variant>
+</Hst.Story>
---
+++ apps/frontend/src/lib/components/Button/index.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { cn } from "@/lib/cn";
+	import type { HTMLButtonAttributes } from "svelte/elements";
+
+	interface $$Props extends HTMLButtonAttributes {
+		className?: string;
+		variant?: "unstyled" | "primary" | "accent";
+	}
+
+	export let className = "";
+	export let variant: "unstyled" | "primary" | "accent" = "primary";
+
+	const primaryStyle = cn("bg-secondary");
+	const accentStyle = cn("bg-gradient-to-r", "from-primary", "to-accent");
+	$: additionalStyle =
+		variant === "primary" ? primaryStyle : variant === "accent" ? accentStyle : "";
+</script>
+
+<button
+	class={cn(
+		"text-text02",
+		"hover:text-text",
+		"transition-all",
+		"duration-500",
+		"px-3",
+		"py-2",
+		"flex",
+		"justify-center",
+		"items-center",
+		"rounded-[8px]",
+		additionalStyle,
+		className
+	)}
+	on:click
+	{...$$restProps}
+>
+	<slot />
+</button>
