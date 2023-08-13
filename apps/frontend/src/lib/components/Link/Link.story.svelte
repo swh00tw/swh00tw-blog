0c80e8522f6d78ce678cd66fa683445e0f0a34be
swh00tw <a6140000@gmail.com>
2023-08-12 21:21:27 -0400

    feat(link): finish link component

---
+++ apps/frontend/src/lib/components/Link/Link.story.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Hst } from "@histoire/plugin-svelte";
+	import Link from "$lib/components/Link/index.svelte";
+	import { cn } from "@/lib/cn";
+	export let Hst: Hst;
+
+	let content = "Link";
+</script>
+
+<Hst.Story title="Link">
+	<svelte:fragment slot="controls">
+		<Hst.Text bind:value={content} title="Content" />
+	</svelte:fragment>
+	<Link className={cn("m-4", "text-[32px]")} {content} href="https://swh00tw.me" />
+</Hst.Story>
