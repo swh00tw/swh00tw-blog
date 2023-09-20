bd7aa031d7a3c220663f0394d7d07d7710247697
swh00tw <a6140000@gmail.com>
2023-09-19 20:36:33 -0400

    update cors

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -2,7 +2,7 @@ import express, { Express } from "express";
 import cors from "cors";
 
 const corsOptions = {
-  origin: ["*"],
+  origin: ["http://localhost:5173", "https://swh00tw-blog.vercel.app"],
 };
 
 const app: Express = express();
