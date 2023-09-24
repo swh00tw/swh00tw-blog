81f5db6c3e5e5e82fab18bc9dcc986c78493f159
swh00tw <a6140000@gmail.com>
2023-09-24 18:42:27 -0400

    modify backend cors

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,8 +1,16 @@
 import express, { Express } from "express";
 import cors from "cors";
 
+const allowedOrigin = /^https:\/\/swh00tw-blog([a-zA-Z0-9-]+)\.vercel\.app\/$/;
+
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://swh00tw-blog.vercel.app"],
+  origin: (origin, callback) => {
+    if (origin === "http://localhost:5173" || allowedOrigin.test(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
 };
 
 const app: Express = express();
