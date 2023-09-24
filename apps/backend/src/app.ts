5adcf9908ee088c39305876b322d030ad6a19413
swh00tw <a6140000@gmail.com>
2023-09-24 19:24:53 -0400

    fix cors

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -2,11 +2,7 @@ import express, { Express } from "express";
 import cors from "cors";
 
 const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "https://swh00tw-blog.vercel.app",
-    /^https:\/\/swh00tw-blog-49mjzhxmq-swh00tw\.vercel\.app\/([a-zA-Z0-9-?=_]+)$/,
-  ],
+  origin: ["http://localhost:5173", "https://swh00tw-blog.vercel.app"],
 };
 
 const app: Express = express();
