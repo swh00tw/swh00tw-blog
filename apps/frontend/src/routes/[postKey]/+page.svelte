b6ca7e32014583d90178c62a7094d2977d9ad0b0
swh00tw <a6140000@gmail.com>
2023-09-25 18:34:35 -0400

    fix(og): encode uri

--- apps/frontend/src/routes/[postKey]/+page.svelte
+++ apps/frontend/src/routes/[postKey]/+page.svelte
@@ -67,8 +67,14 @@
 	<meta
 		property="og:image"
 		content={`https://swh00tw-blog.vercel.app/og?${
-			$PagePost?.data?.Post?.title ? `title=${$PagePost?.data?.Post?.title}` : ""
-		}${$PagePost?.data?.Post?.description ? `&desc=${$PagePost?.data?.Post?.description}` : ""}`}
+			$PagePost?.data?.Post?.title
+				? `title=${encodeURIComponent($PagePost?.data?.Post?.title)}`
+				: ""
+		}${
+			$PagePost?.data?.Post?.description
+				? `&desc=${encodeURIComponent($PagePost?.data?.Post?.description)}`
+				: ""
+		}`}
 	/>
 </svelte:head>
 
