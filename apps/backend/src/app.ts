abeedadd1d5da50eb86f5124ff14ac25f8ca8286
swh00tw <a6140000@gmail.com>
2023-10-13 00:51:07 -0400

    add cache

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -7,5 +7,18 @@ const corsOptions = {
 
 const app: Express = express();
 app.use(cors(corsOptions));
+// cache control
+app.use(function (req, res, next) {
+  // here you can define period in second, this one is 5 minutes
+  const period = 60 * 5;
+  // you only want to cache for GET requests
+  if (req.method == "GET") {
+    res.set("Cache-control", `public, max-age=${period}`);
+  } else {
+    // for the other requests set strict no caching parameters
+    res.set("Cache-control", `no-store`);
+  }
+  next();
+});
 
 export default app;
