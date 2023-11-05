38d2bd3d175af4e50a7a551114506e94ec1ff655
swh00tw <a6140000@gmail.com>
2023-11-05 17:27:47 -0500

    feat(highligher): add shiki

--- apps/frontend/svelte.config.js
+++ apps/frontend/svelte.config.js
@@ -1,10 +1,18 @@
 import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/kit/vite";
-import { mdsvex } from "mdsvex";
+import { mdsvex, escapeSvelte } from "mdsvex";
+import shiki from "shiki";
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
-	extensions: [".md"]
+	extensions: [".md"],
+	highlight: {
+		highlighter: async (code, lang = "text") => {
+			const highlighter = await shiki.getHighlighter({ theme: "poimandres" });
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang }));
+			return `{@html \`${html}\` }`;
+		}
+	}
 };
 
 /** @type {import('@sveltejs/kit').Config} */
