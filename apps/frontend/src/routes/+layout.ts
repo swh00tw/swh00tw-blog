02d7d0b280058edddde991d6ac2dcaa172d082de
swh00tw <a6140000@gmail.com>
2024-01-03 00:07:20 +0800

    feat: add vercel analytics

--- apps/frontend/src/routes/+layout.ts
+++ apps/frontend/src/routes/+layout.ts
@@ -1,4 +1,8 @@
 import type { LayoutLoadEvent } from "./$types";
+import { dev } from "$app/environment";
+import { inject } from "@vercel/analytics";
+
+inject({ mode: dev ? "development" : "production" });
 
 export const config = {
 	isr: {
